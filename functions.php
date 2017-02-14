ccbe4cceba17eb569ea4794d43b887385ecd5c62
Timothy Russell <githubber@timrussell.com>
2017-02-14 07:26:55 -0800

    preventing height image cropping

--- functions.php
+++ functions.php
@@ -211,8 +211,8 @@ add_filter( 'max_srcset_image_width', create_function( '', 'return 1;' ) );
  * @link https://divibooster.com/stop-divi-from-cropping-feature-post-heights/
  *
  */
-add_filter('et_theme_image_sizes', 'yourprefix_remove_featured_post_cropping');
-function yourprefix_remove_featured_post_cropping($sizes) {
+add_filter('et_theme_image_sizes', 'ro_remove_featured_post_cropping');
+function ro_remove_featured_post_cropping($sizes) {
 	if (isset($sizes['1080x675'])) {
 		unset($sizes['1080x675']);
 		$sizes['1080x9998'] = 'et-pb-post-main-image-fullwidth';
